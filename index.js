55aa892be6c2c7114b52edfc0fcf9cdb75828a4d
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-15 18:30:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function happyHolidaysTo(name){
   return `Happy holidays,${name}!`;
 }
 
-function holidayCountdown()
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
